ea0024d38e489834086942c898e06a9a0b29052f
Ed Shevtsov <e.shevtsov@unitedtraders.com>
2023-10-04 19:47:09 +0200

    fix: check deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Let&lsquo;s check an auto deploy <br />
+          Let&lsquo;s check an auto deploy 2 <br />
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
